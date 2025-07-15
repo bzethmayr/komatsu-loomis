95aa2abcf18ec85237fa6dbd1281cf9dcb6d2202
Benjamin Zethmayr <gribnitt@gmail.com>
2025-07-14 22:41:12 -0500

    2 of 2 of 1 of 3

--- komatsu-test/core/src/main/java/com/bzethmayr/komatsu/test/core/schedulers/PublishedPageProcessingTask.java
+++ komatsu-test/core/src/main/java/com/bzethmayr/komatsu/test/core/schedulers/PublishedPageProcessingTask.java
@@ -1,10 +1,22 @@
 package com.bzethmayr.komatsu.test.core.schedulers;
 
+import com.day.cq.search.Predicate;
+import com.day.cq.search.PredicateGroup;
+import com.day.cq.search.Query;
+import com.day.cq.search.QueryBuilder;
+import com.day.cq.search.result.Hit;
+import com.day.cq.search.result.SearchResult;
+import org.apache.sling.api.resource.*;
 import org.apache.sling.commons.scheduler.Scheduler;
 import org.apache.sling.settings.SlingSettingsService;
 import org.osgi.service.component.annotations.*;
 
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import java.util.Date;
 import java.util.Optional;
+
+import static java.util.Collections.singletonMap;
 
 @Component(
         service = PublishedPageProcessingTask.class,
@@ -19,13 +31,20 @@ public class PublishedPageProcessingTask implements Runnable {
     private SlingSettingsService settings;
     private Scheduler scheduler;
     private boolean isScheduled;
+    // state and related methods to here are early candidates for a superclass
+    private ResourceResolverFactory resolvers;
+    private QueryBuilder queries;
 
     public PublishedPageProcessingTask(
             final SlingSettingsService settings,
-            final Scheduler scheduler
+            final Scheduler scheduler,
+            final ResourceResolverFactory resolvers,
+          final QueryBuilder queries
     ) {
         this.settings = settings;
         this.scheduler = scheduler;
+        this.resolvers = resolvers;
+        this.queries = queries;
     }
 
     boolean onAuthor() {
@@ -75,7 +94,70 @@ public class PublishedPageProcessingTask implements Runnable {
         }
 
         // ... on with it
-        
+        // https://developer.adobe.com/experience-manager/reference-materials/6-5/javadoc/com/day/cq/search/QueryBuilder.html
+        // and I need a resource resolver. fun.
+        try (final ResourceResolver resolver = resolvers.getServiceResourceResolver(singletonMap(
+                ResourceResolverFactory.SUBSERVICE, "post-publish"))) {
+            final SearchResult result = obtainListingHandle(resolver);
+            processAllResults(result, resolver);
+
+        } catch (final LoginException authe) {
+            throw new RuntimeException(authe);
+        }
+    }
+
+    protected void processAllResults(final SearchResult result, final ResourceResolver resolver) {
+        result.getHits().forEach(hit -> processOneResult(hit, resolver));
+    }
+
+    protected void processOneResult(final Hit hit, final ResourceResolver resolver) {
+        try {
+            final Resource content = hit.getResource().getChild("jcr:content");
+            final ModifiableValueMap edits = content.adaptTo(ModifiableValueMap.class);
+            if (edits == null) {
+                throw new RuntimeException("Cannot adapt for editing");
+            }
+            edits.put("lastProcessed", new Date());
+            resolver.commit();
+        } catch (final RepositoryException | PersistenceException re) {
+            throw new RuntimeException(re);
+        }
+    }
+
+    protected SearchResult obtainListingHandle(final ResourceResolver resolver) {
+        // cq:Page
+        // visible under /content
+        // cq:Page/jcr:content.cq:lastReplicated
+        // > processedDate
+        // or there is no such property as processedDate at all
+        // better to place new property "processedDate" in jcr:content if we can, most likely
+        final PredicateGroup top = new PredicateGroup();
+        top.setAllRequired(false);
+        final Predicate isContent = new Predicate("path");
+        isContent.set("path", "jcr:root/content");
+        final Predicate isPage = new Predicate("type");
+        isPage.set("type", "cq:Page");
+        final Predicate recent = new Predicate("dateComparison");
+        recent.set("property1", "jcr:content/cq:lastReplicated");
+        recent.set("property2", "jcr:content/lastProcessed");
+        final PredicateGroup newlyPublished = new PredicateGroup();
+        newlyPublished.add(isContent);
+        newlyPublished.add(isPage);
+        newlyPublished.add(recent);
+        top.add(newlyPublished);
+        final PredicateGroup neverProcessed = new PredicateGroup();
+        neverProcessed.add(isContent);
+        neverProcessed.add(isPage);
+        final Predicate notProcessed = new Predicate("property");
+        notProcessed.set("property", "jcr:content/lastProcessed");
+        notProcessed.set("operation", "exists");
+        notProcessed.set("value", "false");
+        neverProcessed.add(notProcessed);
+        top.add(neverProcessed);
+        final Query query = queries.createQuery(top, resolver.adaptTo(Session.class));
+        query.setHitsPerPage(-1);
+        final SearchResult result = query.getResult();
+        return result;
     }
 
     /**
--- komatsu-test/core/src/test/java/com/bzethmayr/komatsu/test/core/schedulers/PublishedPageProcessingTaskTest.java
+++ komatsu-test/core/src/test/java/com/bzethmayr/komatsu/test/core/schedulers/PublishedPageProcessingTaskTest.java
@@ -1,6 +1,14 @@
 package com.bzethmayr.komatsu.test.core.schedulers;
 
+import com.day.cq.search.Query;
+import com.day.cq.search.QueryBuilder;
+import com.day.cq.search.result.Hit;
+import com.day.cq.search.result.SearchResult;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.api.resource.ModifiableValueMap;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.commons.scheduler.ScheduleOptions;
 import org.apache.sling.commons.scheduler.Scheduler;
 import org.apache.sling.settings.SlingSettingsService;
@@ -8,30 +16,47 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 import static com.bzethmayr.komatsu.test.core.schedulers.PublishedPageProcessingTask.*;
 import static java.util.Collections.singleton;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
+/**
+ * Create a service that will run once every 2 minutes and only on the author environment.
+ * This service will find all pages that have been published and will set a property named processedDate to the current time.
+ */
 @ExtendWith(AemContextExtension.class)
 public class PublishedPageProcessingTaskTest {
 
     private SlingSettingsService settings;
     private Scheduler scheduler;
     private PublishedPageProcessingTask underTest;
+    private ResourceResolverFactory resolvers;
+    private QueryBuilder queries;
 
     @BeforeEach
     void setUpUnderTest() {
         settings = mock(SlingSettingsService.class);
         scheduler = mock(Scheduler.class);
-        underTest = new PublishedPageProcessingTask(settings, scheduler);
+        resolvers = mock(ResourceResolverFactory.class);
+        queries = mock(QueryBuilder.class);
+        underTest = new PublishedPageProcessingTask(settings, scheduler, resolvers, queries);
+    }
+
+    void setUpNullReferences() {
+        underTest = new PublishedPageProcessingTask(null, null, null, null);
     }
 
     @Test
     void onAuthor_givenNullReferences_returnsFalse() {
-        underTest = new PublishedPageProcessingTask(null, null);
+        setUpNullReferences();
 
         assertThat(underTest.onAuthor(), is(false));
     }
@@ -77,7 +102,7 @@ public class PublishedPageProcessingTaskTest {
 
     @Test
     void activate_givenNullReferences_doesNothing() {
-        underTest = new PublishedPageProcessingTask(null, null);
+        setUpNullReferences();
 
         underTest.activate();
 
@@ -105,7 +130,7 @@ public class PublishedPageProcessingTaskTest {
 
     @Test
     void deactivate_givenNullReferences_doesNothing() {
-        underTest = new PublishedPageProcessingTask(null, null);
+        setUpNullReferences();
 
         underTest.deactivate();
 
@@ -151,5 +176,50 @@ public class PublishedPageProcessingTaskTest {
     void run_whenNotAuthor_throwsIllegalStateException() {
         assertThrows(IllegalStateException.class, () ->
                 underTest.run());
+    }
+
+    final ResourceResolver setUpOpensResolver() {
+        final ResourceResolver resolver = mock(ResourceResolver.class);
+        assertDoesNotThrow(() ->
+                doReturn(resolver).when(resolvers).getServiceResourceResolver(anyMap()));
+        return resolver;
+    }
+
+    final Query setUpCreatesQuery() {
+        final Query query = mock(Query.class);
+        doReturn(query).when(queries).createQuery(any(), any());
+        return query;
+    }
+
+    final void setUpAdaptableResults(final Query query, final int count) {
+        final SearchResult result = mock(SearchResult.class);
+        final List<Hit> hits = new ArrayList<>(count);
+        IntStream.range(0, count).forEach(x -> {
+            final ModifiableValueMap values = mock(ModifiableValueMap.class);
+            final Resource contentResource = mock(Resource.class);
+            final Resource hitResource = mock(Resource.class);
+            final Hit hit = mock(Hit.class);
+            assertDoesNotThrow(() ->
+                    doReturn(hitResource).when(hit).getResource());
+            doReturn(contentResource).when(hitResource).getChild("jcr:content");
+            doReturn(values).when(contentResource).adaptTo(ModifiableValueMap.class);
+            hits.add(hit);
+        });
+        doReturn(hits).when(result).getHits();
+        doReturn(result).when(query).getResult();
+    }
+
+    @Test
+    void run_whenAuthor_givenSomeResults_updatesEachResult() {
+        setUpIsAuthor();
+        final ResourceResolver resolver = setUpOpensResolver();
+        final Query query = setUpCreatesQuery();
+        setUpAdaptableResults(query, 3);
+
+        underTest.run();
+
+        assertDoesNotThrow(() ->
+                verify(resolver, times(3)).commit());
+        verify(resolver).close();
     }
 }
